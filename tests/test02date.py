d0e08def8931896c5c9c4008524206e0ec7082d6
rg3915 <rg3915@yahoo.com.br>
2014-09-27 02:10:09 -0300

    date test

---
+++ tests/test02date.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+import unittest
+import datetime
+
+
+class DatePattern:
+
+    def __init__(self, year, month, day):
+        self.date = datetime.date(year, month, day)
+        self.year = year
+        self.month = month
+        self.day = day
+
+    def matches(self, date):
+        return self.date == date
+
+
+class MatchTest(unittest.TestCase):
+
+    def testMatches(self):
+        p = DatePattern(2014, 9, 27)
+        self.assertEqual(datetime.date.today(), p)
+
+if __name__ == '__main__':
+    unittest.main()
