2d18a5c314b41c42ba1e8a0bdf6fd92dbe3146f5
rg3915 <rg3915@yahoo.com.br>
2014-09-28 23:03:54 -0300

    test random passando

--- tests/test_random.py
+++ tests/test_random.py
@@ -5,7 +5,7 @@ import unittest
 
 def gen_age():
     # gera numeros inteiros entre 15 e 99
-    return range(15, 99)
+    return random.randint(15, 99)
 
 
 class AgeTest(unittest.TestCase):
@@ -13,13 +13,9 @@ class AgeTest(unittest.TestCase):
     def setUp(self):
         self.a = gen_age()
 
-    def test_choice(self):
-        element = random.choice(self.a)
-        self.assertTrue(element in self.a)
+    def test_gen_age(self):
+        self.assertTrue(self.a >= 15 and self.a <= 99)
 
-    def test_sample(self):
-        for element in random.sample(self.a, 5):
-            self.assertTrue(element in self.a)
 
 if __name__ == '__main__':
     unittest.main()
